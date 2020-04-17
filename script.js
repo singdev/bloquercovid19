0b2cfa44fea145fe263311d2dd79bb419f727841
Nve Hounkponou Orphée <norpheehounnkponou@gmail.com>
2020-04-17 12:05:56 +0100

    FIX remove stat Gabon

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ window.addEventListener('load', () => {
 
         });
     })
-
-    fetchCovid19Data();
 })
 
 function toggleShowDropdown(e){
@@ -50,23 +48,3 @@ document.querySelector(".custom-link").addEventListener('click', function (event
       event.preventDefault();
     }
 });
-
-function fetchCovid19Data(){
-    fetch('https://app.bloquercovid19.com/bat/api').then(response => response.json())
-    .then(json => {
-        if(json){
-            const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novemnbre", "Décembre"];
-
-            const result = json.data.find(s =>  s.Country_Region == "Gabon");
-            console.log(result);
-            const date = new Date(result.Last_Update);
-            document.querySelector('#confirmed').innerHTML = "Gabon: " + result.Confirmed;
-            document.querySelector('#deces').innerHTML = result.Deaths;
-            document.querySelector('#stat-cas-gabon').innerHTML = result.Confirmed;
-            document.querySelector("#stat-date-gabon").innerHTML = "Mise à jour le " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
-
-        }
-    }).catch(err => {
-        console.log(err);
-    });
-}
